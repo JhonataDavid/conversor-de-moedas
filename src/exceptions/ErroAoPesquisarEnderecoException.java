23ab5c7357aab28d0c26802d599d1f610f09acad
jhonatadavid <jhonatadavidf@gmail.com>
2025-05-14 14:57:42 -0300

    Remove package das exceptions.

--- src/exceptions/ErroAoPesquisarEnderecoException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class ErroAoPesquisarEnderecoException extends RuntimeException {
-    public ErroAoPesquisarEnderecoException(String message) {
-        super(message);
-    }
-}
